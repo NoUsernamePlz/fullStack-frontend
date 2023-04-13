e1a8cd8074c5a04ee47db3ac1cf2d3eb126b4d0a
Komal <sandilyaaparagita@gmail.com>
2023-04-13 18:20:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header';
+import Content from './component/Content';
+import Main from './component/Main';
+import Signup from './component/Signup';
+import Login from './component/Login';
+import { BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom';
 
 function App() {
+  const user = localStorage.getItem("token");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+     
+        <Routes>
+        {user && <Route path="/" exact element={<Main />} />}
+          <Route path='/content' Component={Content}/>
+          <Route path='/signup' Component={Signup}/>
+          <Route path='/login' Component={Login}/>
+          <Route path="/" element={<Navigate replace to="/content" />} />
+        </Routes>
+      
+     
     </div>
   );
 }
 
 export default App;
+
+
